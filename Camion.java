38e2d03158c8c28266c772c42d456f24c80adbf4
Luis-Angel-G <langelgirona@gmail.com>
2024-10-28 23:36:38 -0600

    Se agrega Javadoc

--- Camion.java
+++ Camion.java
@@ -1,6 +1,19 @@
+/**
+ * @author Luis Ángel Girón Arévalo
+ * @date Fecha de creación: 2024-10-24
+ * @date Fecha de última modificación: 2024-10-28
+ * Descripción: Clase que representa un camión.
+ */
 public class Camion extends Transporte {
-    public double descuento;
+    public double descuento; // Descuento aplicado al costo
 
+    /**
+     * Constructor para la clase Camion.
+     *
+     * @param capacidad La capacidad del camión.
+     * @param distancia La distancia a recorrer.
+     * @param descuento El descuento a aplicar.
+     */
     public Camion(double capacidad, double distancia, double descuento) {
         super(capacidad, distancia);
         this.descuento = descuento;
@@ -8,14 +21,29 @@ public class Camion extends Transporte {
         setCapacidadmax(1000);
     }
 
+    /**
+     * Obtiene el descuento aplicado al costo.
+     *
+     * @return Descuento aplicado.
+     */
     public double getDescuento() {
         return descuento;
     }
 
-    public void setDescuento() {
-        this.descuento = 5;
+    /**
+     * Establece el descuento aplicado al costo.
+     *
+     * @param descuento Descuento a establecer.
+     */
+    public void setDescuento(double descuento) {
+        this.descuento = descuento;
     }
 
+    /**
+     * Calcula el costo del camión considerando el descuento.
+     *
+     * @return El costo total del camión.
+     */
     @Override
     public double calcularCosto() {
         double costo = super.calcularCosto();
