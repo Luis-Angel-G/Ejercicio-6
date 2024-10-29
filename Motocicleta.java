d68078001812ca14a68e91ed443ab9014dfa5b5b
Luis-Angel-G <langelgirona@gmail.com>
2024-10-28 20:12:45 -0600

    Se agregan metodos al constructor

--- Motocicleta.java
+++ Motocicleta.java
@@ -1,5 +1,7 @@
 public class Motocicleta extends Transporte {
-    public Motocicleta(double tarifabase, double capacidad, double distancia, int capacidadmax) {
+    public Motocicleta(double capacidad, double distancia) {
         super(tarifabase, capacidad, distancia, capacidadmax);
+        setTarifabase(10);
+        setCapacidadmax(50);
     }
 }
