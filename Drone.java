7c2f8d8d5bfed6d03e80903ec4f92c45b03d15d3
Luis-Angel-G <langelgirona@gmail.com>
2024-10-28 23:37:51 -0600

    Se agrega Javadoc

--- Drone.java
+++ Drone.java
@@ -1,6 +1,19 @@
+/**
+ * @author Luis Ángel Girón Arévalo
+ * @date Fecha de creación: 2024-10-24
+ * @date Fecha de última modificación: 2024-10-28
+ * Descripción: Clase que representa un dron.
+ */
 public class Drone extends Transporte {
-    public int limite;
+    public int limite; // Límite de distancia para el dron
 
+    /**
+     * Constructor para la clase Drone.
+     *
+     * @param capacidad La capacidad del dron.
+     * @param distancia La distancia a recorrer.
+     * @param limite El límite de distancia permitido.
+     */
     public Drone(double capacidad, double distancia, int limite) {
         super(capacidad, distancia);
         this.limite = limite;
@@ -8,14 +21,30 @@ public class Drone extends Transporte {
         setCapacidadmax(5);
     }
 
+    /**
+     * Obtiene el límite de distancia permitido.
+     *
+     * @return Límite de distancia permitido.
+     */
     public int getLimite() {
         return limite;
     }
 
-    public void setLimite() {
-        this.limite = 10;
+    /**
+     * Establece el límite de distancia permitido.
+     *
+     * @param limite Límite a establecer.
+     */
+    public void setLimite(int limite) {
+        this.limite = limite;
     }
 
+    /**
+     * Valida si la entrega del dron es válida.
+     *
+     * @return true si la entrega es válida, false en caso contrario.
+     * @throws EntregaInvalidaExcepcion si la entrega no es válida.
+     */
     @Override
     public boolean validarEntrega() throws EntregaInvalidaExcepcion {
         if (capacidad > capacidadmax || distancia > limite) {
