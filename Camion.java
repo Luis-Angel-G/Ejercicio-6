6c9dac7d7c8f33e5f44cda7c555a102c87f9e7e4
Luis-Angel-G <langelgirona@gmail.com>
2024-10-28 19:10:20 -0600

    Se comienza Camion.java

---
+++ Camion.java
@@ -0,0 +1,10 @@
+public class Camion extends Transporte {
+    public double descuento;
+
+    public Camion(double tarifabase, double capacidad, double distancia, int capacidadmax, double descuento) {
+        super(tarifabase, capacidad, distancia, capacidadmax);
+        this.descuento = descuento;
+    }
+
+    p
+}
