c001f3fb37b6514cd80df4f25f84dc54d84c7555
Luis-Angel-G <langelgirona@gmail.com>
2024-10-28 23:37:22 -0600

    Se agrega Javadoc

--- Motocicleta.java
+++ Motocicleta.java
@@ -1,4 +1,16 @@
+/**
+ * @author Luis Ángel Girón Arévalo
+ * @date Fecha de creación: 2024-10-24
+ * @date Fecha de última modificación: 2024-10-28
+ * Descripción: Clase que representa una motocicleta.
+ */
 public class Motocicleta extends Transporte {
+    /**
+     * Constructor para la clase Motocicleta.
+     *
+     * @param capacidad La capacidad de la motocicleta.
+     * @param distancia La distancia a recorrer.
+     */
     public Motocicleta(double capacidad, double distancia) {
         super(capacidad, distancia);
         setTarifabase(10);
